b272b7ccf9f125cc148daa9347665e60376a65bc
Ivan_Andrianov <andreyanovi@yandex.ru>
2023-10-25 00:43:04 +0300

    Initial upload to coordinate the development of the project

---
+++ users/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    MEMBER = 'Member', 'Участник'
+    ADMIN = 'Admin', 'Администратор'
+    MODERATOR = 'Moderator', 'Модератор'
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='почта')
+    phone = models.CharField(max_length=35, verbose_name='Номер телефона', blank=True, null=True)
+    country = models.CharField(max_length=35, verbose_name='Город', blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', blank=True, null=True)
+    roles = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
+
+    def __str__(self):
+        return self.username
+
+    def is_paid_user(self):
+        return self.paid_users.exists()
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
